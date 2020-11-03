102a680896eefda5a22cd978c570576afbb33b02
Supriya2301 <63453592+Supriya2301@users.noreply.github.com>
2020-11-03 12:35:38 +0530

    Update Typing Speed Test.py

--- Typing Speed Test.py
+++ Typing Speed Test.py
@@ -1,10 +1,13 @@
+# Importing tkinter and timeit libraries
 from tkinter import *
 import random
 from tkinter import messagebox
 from timeit import default_timer as timer 
-import time
 
+# Creating  first Window
 window = Tk()
+
+# Initialising needed variables
 count = 0
 score = 0
 miss = 0
@@ -15,69 +18,69 @@ correct_count =0
 timeleft = 60
 sliderWords = ''
 
-
+# creating Label for the title in first window
 lbl = Label(window,text = 'TYPING SPEED TEST',font=('arial',25,'italic bold'),bg = 'black',fg='white')
 lbl.place(x=200,y=10)
         
-
+# Creating Beginner Function
 def beginner():
-    root = Tk()
-    t0 = timer()
-    root.geometry('800x600+400+100')
-    root.configure(bg = 'black')
+    root = Tk()             # creating second window for beginner level
+    t0 = timer()            # Timer will be started 
+    root.geometry('800x600+400+100')     # setting the geometry of the window, height=800,width=600,x-axis=400,y axis= 100
+    root.configure(bg = 'black')          # configuring background of the window
     
-    root.title('Typing Speed Test Game')
+    root.title('Typing Speed Test Game')    # Giving the title 
 
-    root.iconbitmap("image.ico")
+    root.iconbitmap("image.ico")            # Changing icon
  
 
     
-    def labelSlider():
+    def labelSlider():            # Creating function for sliding the title
         global count,sliderWords
-        text = 'Welcome to Typing Speed Game'
-        if(count>=len(text)):
+        text = 'Welcome to Typing Speed Game'       # initialising text variable with project name 
+        if(count>=len(text)):                # checking condition 
             
             count = 0
             sliderWords = ''
-        sliderWords += text[count]
-        count += 1
-        fontLabel.configure(text = sliderWords)
-        fontLabel.after(100,labelSlider)
+        sliderWords += text[count]          # if the condition is false,then add text[count] in sliderwords
+        count += 1                          # incrementing count
+        fontLabel.configure(text = sliderWords)       # configuring fontLabel with sliderwords
+        fontLabel.after(100,labelSlider)         # after every 100 ms, it will call the labelSlider function.
 
 
    
    
-    def get_sentence():
+    def get_sentence():                  # creating function to get words using file handling
         f = open("beginner.txt","r")
-        lines = f.read().split("\n")
-        line = random.choice(lines)
+        lines = f.read().split("\n")     # contains words in list wherever finds next line
+        line = random.choice(lines)       # randomly picking word from list
         return line
     
 
    
     
-    def startGame(event):
+    def startGame(event):             # Creating function using event handling
         global score,miss,timeleft,l,count
         if (timeleft==60):
-             time()
-        gamePlayDetailLabel.configure(text = '')
-        if (wordEntry.get()==wordLabel['text']):
+             time()              # when timeleft is 60,it will call the time function
+        gamePlayDetailLabel.configure(text = '')        # once the game has started the hint that "enter and hit" will vanish
+        if (wordEntry.get()==wordLabel['text']):      # condition to check whether user has given the correct inout or not
             
-            score += 1
-            l.append(wordEntry.get())
-            l1.append(wordEntry.get())
-            scoreLabelCount.configure(text = score)
+            score += 1            # incrementing score
+            l.append(wordEntry.get())         # append the input in list l
+            l1.append(wordEntry.get())        # append the input in list l1
+            scoreLabelCount.configure(text = score)     # configuring the label with score
            
 
-        else:
+        else:             # when condition is false
             
             
-            miss += 1
-            l.append(wordEntry.get())
-        wordLabel.configure(text=get_sentence())
-        wordEntry.delete(0,END)
+            miss += 1           # incrementing the variable miss
+            l.append(wordEntry.get())         # appending the input in list l
+        wordLabel.configure(text=get_sentence())       # configuring the Wordlabel with function get_sentence()
+        wordEntry.delete(0,END)            # clear the entry box after hitting Enter
 
-    def time():
+    def time():           # creating time function
          
          
          global timeleft,score,miss,count,correct_count
@@ -86,26 +89,28 @@ def beginner():
              pass
          else:
              
-             timeLabelCount.configure(fg = 'red')
+             timeLabelCount.configure(fg = 'red')    # when timeleft<11 , fg of timelabelcount will be changed to red
          if(timeleft>0):
              
-             timeleft -= 1
-             timeLabelCount.configure(text = timeleft)   # to configure the count woth left time
-             timeLabelCount.after(1000,time)    # after every 1000 micro sec..it will call time
+             timeleft -= 1       # decrementing timeleft 
+             timeLabelCount.configure(text = timeleft)   # to configure the count with left time
+             timeLabelCount.after(1000,time)    # after every 1000 mili sec..it will call time
          else:
-             t1 = timer()
-             for i in l:
-                 count += len(i)
-             print(l)
+             t1 = timer()        # timer will be stopped when timeleft becomes 0
+             for i in l:         # traversing the list l
+                 count += len(i)   # adding the length of each word in list
+             print(l)               # list l contains all input
              print(count)
-             print(l1)
-             for i in l1:
-                 correct_count += len(i)
-             print(correct_count)    
-             t = str(round(t1-t0))   
+             print(l1)            # list l1 contains only correct input
+             for i in l1:          # traversing the list l1
+                 correct_count += len(i)         
+             print(correct_count)        # printing the total length of correct input  by user 
+             t = str(round(t1-t0))        # total time taken by user
+                
+                # configuring the label with Total Score , WPM and Accuracy
              gamePlayDetailLabel.configure(text = 'Total Score = {} | WPM={} | Accuracy={}'.format(score-miss,int((count*60)/int(5*(t1-t0))),int((correct_count*100)/count)))
-             rr = messagebox.askretrycancel('Notification','Hit Retry to Play Again')
-             if rr == True:
+             rr = messagebox.askretrycancel('Notification','Hit Retry to Play Again')    # asking for Retry or Cancel
+             if rr == True:          # if user has selected Retry ,then set all the variables as earlier
                  
                  score = 0
                  timeleft = 60
@@ -115,11 +120,11 @@ def beginner():
                  scoreLabelCount.configure(text = score)
 
   
-
+# Creating Different Labels
          
     fontLabel = Label(root,text = 'Welcome to Typing Speed Game',font=('arial',25,'italic bold'),bg ='black',fg = 'grey',width = 40)
     fontLabel.place(x = 10,y = 10)
-    labelSlider()
+    labelSlider()         # calling labelSlider function
     
 
     
@@ -140,13 +145,15 @@ def beginner():
 
     gamePlayDetailLabel = Label(root, text = 'Type Word and Hit Enter',font = ('arial',20,'italic'),bg = 'black', fg = 'dark grey')
     gamePlayDetailLabel.place(x = 220,y = 400)
+     
+        # Creating Entry Box
 
     wordEntry = Entry(root,font=('arial',20,'italic bold'),bd = 10,width = 15,justify='center') # justify enable the text to be input in center
     wordEntry.place(x = 300,y = 300)
-    wordEntry.focus_set()  # by this no need to enter into the entry box and then write...enable the input wothout clicking
+    wordEntry.focus_set()  # by this no need to enter into the entry box and then write...enable the input wothout clicking into the entry box
 
 
-    root.bind('<Return>',startGame) # binding enter with the function call .. we will be able to get the words typed after hitting enter.part of event handling
+    root.bind('<Return>',startGame) # binding enter with the function StartGame .. we will be able to get the words typed after hitting enter.part of event handling
     root.mainloop()
 
 ######### FOR INTERMEDIATE LEVEL ###########
@@ -415,10 +422,12 @@ def advanced():
                  
     
 
-    
+# Creating label     
              
 lbl=Label(window,text="Select Your Level",font=('arial',25,'italic bold'),bg ='black',fg = 'brown')
 lbl.place(x=250,y=100)
+
+# Creating 3 different buttons for 3 level
 btn=Button(window,text="Beginner",command=beginner,bg='orange red',width=10,height=2,font=('arial',10,'italic bold'))
 btn.place(x=350,y=150)
 btn=Button(window,text="Intermediate",command = intermediate, bg='orange red',width=10,height=2,font=('arial',10,'italic bold'))
@@ -426,18 +435,19 @@ btn.place(x=350,y=200)
 btn=Button(window,text="Advanced",bg='orange red',command = advanced,width=10,height=2,font=('arial',10,'italic bold'))
 btn.place(x=350,y=250)
 
+# setting iMage into Label for first Window
 filename = PhotoImage(file = "image2.png")
 imglabel = Label(window,image=filename,width=770)
 imglabel.place(x=10,y= 300)
 
 
 
-window.geometry('800x600+400+100')
-window.configure(bg ='black')
-window.title('Typing Speed Test Game')
-window.iconbitmap('image.ico')
+window.geometry('800x600+400+100')     # setting geometry
+window.configure(bg ='black')           # changing bg
+window.title('Typing Speed Test Game')  # setting title
+window.iconbitmap('image.ico')      # changing icon
 
-window.mainloop()
+window.mainloop()         
 
         
 
